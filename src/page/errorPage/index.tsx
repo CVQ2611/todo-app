f82fb7d16d42dc356db2716b809bd5a3569bc9b8
CVQ2611 <vuquangchung3@gmail.com>
2022-12-25 08:05:32 +0700

    fix error page

--- src/page/errorPage/index.tsx
+++ src/page/errorPage/index.tsx
@@ -1,13 +1,19 @@
 import { Result } from "antd";
 import Button from "../../components/button";
-
+import classNames from "classnames/bind";
+import styles from './errorPage.module.css';
+const cx = classNames.bind(styles)
 function ErrorPage() {
     return (
         <Result
             status="500"
             title="500"
             subTitle="Coming Soon!."
-            extra={<Button primary to={'/'}>Back Home</Button>}
+            extra={
+                <div className={cx('button-error')}>
+                    <Button primary to={'/'}>Back Home</Button>
+                </div>
+            }
         />
     )
 }
